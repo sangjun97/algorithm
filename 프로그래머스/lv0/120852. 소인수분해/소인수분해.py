ad42e0595575bfb6c70a448a8b4ab23ba792cddb
sangjun97 <77375401+sangjun97@users.noreply.github.com>
2023-01-06 14:20:43 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer=[]
+    i=2
+    while i<=n:
+        if n%i==0:
+            answer.append(i)
+            n=n//i
+            
+        else:
+            i+=1
+    answer=sorted(set(answer))
+    return answer
