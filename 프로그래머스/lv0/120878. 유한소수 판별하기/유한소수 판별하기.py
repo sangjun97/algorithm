d2f88312bd0aef37d5e2161efc871a16c1ad207e
sangjun97 <77375401+sangjun97@users.noreply.github.com>
2023-01-18 16:03:06 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,8 +1,11 @@
 import math
 def solution(a, b):
-    answer = 1
-    c=a/math.gcd(a,b)
+    answer = 2
     d=b/math.gcd(a,b)
-    if len(str(c/d))>=16:
-        answer=2
+    while d%2==0:
+        d=d//2
+    while d%5==0:
+        d=d//5
+    if d==1:
+        answer=1
     return answer
