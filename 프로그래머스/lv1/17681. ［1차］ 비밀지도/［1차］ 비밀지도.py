328b6badac847f243754eb8211e6f45010733da2
sangjun97 <77375401+sangjun97@users.noreply.github.com>
2023-02-22 13:38:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,10 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i,j in zip(arr1,arr2):
+        a=bin(i | j)[2:].zfill(n)
+        a=a.replace('1','#')
+        a=a.replace('0',' ')
+        answer.append(a)
+    # print(a)
+    # print(b)
+    return answer
