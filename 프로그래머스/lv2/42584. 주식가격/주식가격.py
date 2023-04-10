37bcecd063f97b17c473a95b1b88a9e61d67f01a
sangjun97 <77375401+sangjun97@users.noreply.github.com>
2023-04-11 01:44:42 +0900

    [level 2] Title: 주식가격, Time: 35.34 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,13 +1,3 @@
-from collections import deque
 def solution(prices):
     answer = []
-    prices=deque(prices)
-    while prices:
-        num=0
-        p = prices.popleft()
-        for i in prices:
-            num+=1
-            if i<p:
-                break
-        answer.append(num)
     return answer
