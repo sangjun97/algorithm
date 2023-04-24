6b635bdc65c54effda7c1c4fb1af262b1016df88
sangjun97 <77375401+sangjun97@users.noreply.github.com>
2023-04-24 13:55:48 +0900

    [level 2] Title: 구명보트, Time: 12.14 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    p = deque(sorted(people))
+    while p:
+        if len(p)==1:
+            answer+=1
+            break
+        if p[0]+p[-1]<=limit:
+            p.pop()
+            p.popleft()
+        else:
+            p.pop()
+        answer+=1
+    return answer
